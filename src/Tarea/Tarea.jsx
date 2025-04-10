46ab07a00cb8d2e48d59efc9571635fb66a2ada4
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-04-10 18:12:03 +0200

    Modificar Tarea

--- src/Tarea/Tarea.jsx
+++ src/Tarea/Tarea.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tarea.css';
 
 const Tarea = ({ tarea, eliminarTarea, actualizarTarea }) => {
+    const [modoEdicion, setModoEdicion] = useState(false);
+    const [tituloEditado, setTituloEditado] = useState(tarea.titulo);
+    const [descripcionEditada, setDescripcionEditada] = useState(tarea.descripcion);
+
+    const guardarEdicion = () => {
+        actualizarTarea(tarea.id, { titulo: tituloEditado, descripcion: descripcionEditada });
+        setModoEdicion(false);
+    };
+
     return (
         <li className="tarea">
-            <strong>{tarea.titulo}</strong>
-            <p>{tarea.descripcion}</p>
-            <button onClick={() => eliminarTarea(tarea.id)}>Eliminar</button>
-            <button
-                onClick={() =>
-                    actualizarTarea(tarea.id, { completada: !tarea.completada })
-                }
-            >
-                {tarea.completada ? 'Marcar como Incompleta' : 'Marcar como Completa'}
-            </button>
+            {modoEdicion ? (
+                <>
+                    <input
+                        type="text"
+                        value={tituloEditado}
+                        onChange={(e) => setTituloEditado(e.target.value)}
+                    />
+                    <textarea
+                        value={descripcionEditada}
+                        onChange={(e) => setDescripcionEditada(e.target.value)}
+                    />
+                    <button onClick={guardarEdicion}>Guardar</button>
+                    <button onClick={() => setModoEdicion(false)}>Cancelar</button>
+                </>
+            ) : (
+                <>
+                    <div>
+                        <input
+                            type="checkbox"
+                            checked={tarea.completada}
+                            onChange={() =>
+                                actualizarTarea(tarea.id, { completada: !tarea.completada })
+                            }
+                        />
+                        <strong>{tarea.titulo}</strong>
+                    </div>
+                    <p>{tarea.descripcion}</p>
+                    <button onClick={() => eliminarTarea(tarea.id)}>Eliminar</button>
+                    <button onClick={() => setModoEdicion(true)}>Editar</button>
+                </>
+            )}
         </li>
     );
 };
